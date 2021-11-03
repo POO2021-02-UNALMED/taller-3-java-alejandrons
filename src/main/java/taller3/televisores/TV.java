4d5a706f812b79984c2c7dbeab87c2e77a3a4b1a
alejandrons <80337888+alejandrons@users.noreply.github.com>
2021-11-02 23:12:38 -0500

    Solucion

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,118 @@
+
+package taller3.televisores;
+
+
+public class TV {
+
+    private Marca marca;
+    private int canal = 1;
+    private int precio = 500;
+    private boolean estado;
+    private int volumen = 1;
+    private Control control;
+    private static int numTV;
+
+    public TV(Marca marca, boolean estado) {
+        this.marca = marca;
+        this.estado = estado;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(int volumen) {
+        this.volumen = volumen;
+    }
+
+    public Control getControl() {
+        return control;
+    }
+
+    public void setControl(Control control) {
+        this.control = control;
+    }
+
+    public static int getNumTV() {
+        return numTV;
+    }
+    
+    public void turnOn(){
+        this.estado = true;
+    }
+    public void turnOff(){
+        this.estado = false;
+    }
+
+    public boolean getEstado() {
+        return estado;
+    }
+    
+    public void canalUp(){
+        if(estado){
+            if(canal==120){
+                return ;
+            }
+            else{
+                canal++;
+            }
+        }
+    }
+    public void canalDown(){
+        if(estado){
+            if(canal==1){
+                return ;
+            }
+            else{
+                canal--;
+            }
+        }
+    }
+    
+    public void volumenUp(){
+        if(estado){
+            if(volumen==7){
+                return ;
+            }
+            else{
+                volumen++;
+            }
+        }
+    }
+    
+    public void volumenDown(){
+        if(estado){
+            if(volumen==0){
+                return ;
+            }
+            else{
+                volumen--;
+            }
+        }
+    }
+    
+}
